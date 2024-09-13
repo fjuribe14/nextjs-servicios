f8896eb4e9b1d48627e24388d5061341f6632392
Fernando Uribe <furibe@bankingtech.com.ve>
2024-09-13 17:03:45 -0400

    cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:3000/api/:path*",
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
